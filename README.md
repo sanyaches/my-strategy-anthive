01482c4700c40ffb2bed3c996c40034d96097a4b
Vitaly Kezlya <vitaly@kezlya.com>
2018-08-24 10:15:34 -0600

    Mock response and updated instractions

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# python
-Sample AntHive bot in Python3 language
+# Sample AntHive bot - *Python 3*
+
+## Setup
+``
+
+## Build
+From project root
+``
+
+## Run
+From project root
+`python run.py`
+
+# Participating at AntHive.IO
+- Folk/Clone sample bot
+- Replace logic of ...
+- Set your *username* and *lang* in ANTHIVE file
+- Push your code.
+- Link your repo at https://profile.anthive.io/
+- Watch domination of your colony :)
+
--- run.py
+++ run.py
@@ -1,35 +1,26 @@
 #!/usr/bin/env python
 
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-# HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
-
-  # GET
-  def do_GET(self):
-        # Send response status code
+class Handler(BaseHTTPRequestHandler):
+    def do_POST(self):
         self.send_response(200)
-
-        # Send headers
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        # Send message back to client
-        message = "Hello world!"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
+        mockActions = {"18": 12, "19": 10, "20": 24, "21": 13}
+
+        self.wfile.write(bytes(json.dumps(mockActions), "utf8"))
         return
 
-def run():
-  print('starting server...')
 
-  # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8081)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-  print('running server...')
-  httpd.serve_forever()
+def run():
+    print("let's ant")
+    server_address = ('127.0.0.1', 7070)
+    httpd = HTTPServer(server_address, Handler)
+    httpd.serve_forever()
 
 
 run()
